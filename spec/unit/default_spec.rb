88d411a945161b8ac37e7ab1999ea21014bbacc0
Fernando Honig <fernando.honig@rackspace.co.uk>
2014-03-13 15:16:03 +0000

    Added new tests and replaced others to meet rspec requirements

--- spec/unit/default_spec.rb
+++ spec/unit/default_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
 describe 'rackspace_yum::default' do
-  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+  let(:chef_run) do
+    runner = ChefSpec::Runner.new(:step_into => ['repository'])
+    runner.converge('rackspace_yum::default')
+  end
 
   it 'creates rackspace_yum_globalconfig[/etc/yum.conf]' do
     expect(chef_run).to create_rackspace_yum_globalconfig('/etc/yum.conf')
   end
-
 end
